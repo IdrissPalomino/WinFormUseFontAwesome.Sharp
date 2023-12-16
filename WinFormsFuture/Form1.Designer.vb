de309f7c342ddaf288a6f6f45cc677d8a8d83117
Adrian Carrasco <adrian.carrasco@creativeminersoft.pe>
2023-12-15 23:51:39 -0500

    add updates

---
+++ WinFormsFuture/Form1.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PanelMenu = New Panel()
+        btnSetting = New FontAwesome.Sharp.IconButton()
+        btnMarketing = New FontAwesome.Sharp.IconButton()
+        btnCustomers = New FontAwesome.Sharp.IconButton()
+        btnProducts = New FontAwesome.Sharp.IconButton()
+        btnOrders = New FontAwesome.Sharp.IconButton()
+        btnDashboard = New FontAwesome.Sharp.IconButton()
+        PanelLogo = New Panel()
+        ImgHome = New PictureBox()
+        PanelTitleBar = New Panel()
+        btnMinimize = New Button()
+        btnMaximize = New Button()
+        btnExit = New Button()
+        lblFormTitle = New Label()
+        IconCurrentForm = New FontAwesome.Sharp.IconPictureBox()
+        PanelDesktop = New Panel()
+        PictureBox1 = New PictureBox()
+        PanelMenu.SuspendLayout()
+        PanelLogo.SuspendLayout()
+        CType(ImgHome, ComponentModel.ISupportInitialize).BeginInit()
+        PanelTitleBar.SuspendLayout()
+        CType(IconCurrentForm, ComponentModel.ISupportInitialize).BeginInit()
+        PanelDesktop.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PanelMenu
+        ' 
+        PanelMenu.BackColor = Color.FromArgb(CByte(31), CByte(30), CByte(68))
+        PanelMenu.Controls.Add(btnSetting)
+        PanelMenu.Controls.Add(btnMarketing)
+        PanelMenu.Controls.Add(btnCustomers)
+        PanelMenu.Controls.Add(btnProducts)
+        PanelMenu.Controls.Add(btnOrders)
+        PanelMenu.Controls.Add(btnDashboard)
+        PanelMenu.Controls.Add(PanelLogo)
+        PanelMenu.Dock = DockStyle.Left
+        PanelMenu.Location = New Point(0, 0)
+        PanelMenu.Margin = New Padding(4, 3, 4, 3)
+        PanelMenu.Name = "PanelMenu"
+        PanelMenu.Size = New Size(257, 601)
+        PanelMenu.TabIndex = 2
+        ' 
+        ' btnSetting
+        ' 
+        btnSetting.Dock = DockStyle.Top
+        btnSetting.FlatAppearance.BorderSize = 0
+        btnSetting.FlatStyle = FlatStyle.Flat
+        btnSetting.ForeColor = Color.Gainsboro
+        btnSetting.IconChar = FontAwesome.Sharp.IconChar.Tools
+        btnSetting.IconColor = Color.Gainsboro
+        btnSetting.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnSetting.IconSize = 32
+        btnSetting.ImageAlign = ContentAlignment.MiddleLeft
+        btnSetting.Location = New Point(0, 507)
+        btnSetting.Margin = New Padding(4, 3, 4, 3)
+        btnSetting.Name = "btnSetting"
+        btnSetting.Padding = New Padding(12, 0, 23, 0)
+        btnSetting.Size = New Size(257, 69)
+        btnSetting.TabIndex = 6
+        btnSetting.Text = "Setting"
+        btnSetting.TextAlign = ContentAlignment.MiddleLeft
+        btnSetting.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnSetting.UseVisualStyleBackColor = True
+        ' 
+        ' btnMarketing
+        ' 
+        btnMarketing.Dock = DockStyle.Top
+        btnMarketing.FlatAppearance.BorderSize = 0
+        btnMarketing.FlatStyle = FlatStyle.Flat
+        btnMarketing.ForeColor = Color.Gainsboro
+        btnMarketing.IconChar = FontAwesome.Sharp.IconChar.MailBulk
+        btnMarketing.IconColor = Color.Gainsboro
+        btnMarketing.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnMarketing.IconSize = 32
+        btnMarketing.ImageAlign = ContentAlignment.MiddleLeft
+        btnMarketing.Location = New Point(0, 438)
+        btnMarketing.Margin = New Padding(4, 3, 4, 3)
+        btnMarketing.Name = "btnMarketing"
+        btnMarketing.Padding = New Padding(12, 0, 23, 0)
+        btnMarketing.Size = New Size(257, 69)
+        btnMarketing.TabIndex = 5
+        btnMarketing.Text = "Marketing"
+        btnMarketing.TextAlign = ContentAlignment.MiddleLeft
+        btnMarketing.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnMarketing.UseVisualStyleBackColor = True
+        ' 
+        ' btnCustomers
+        ' 
+        btnCustomers.Dock = DockStyle.Top
+        btnCustomers.FlatAppearance.BorderSize = 0
+        btnCustomers.FlatStyle = FlatStyle.Flat
+        btnCustomers.ForeColor = Color.Gainsboro
+        btnCustomers.IconChar = FontAwesome.Sharp.IconChar.User
+        btnCustomers.IconColor = Color.Gainsboro
+        btnCustomers.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnCustomers.IconSize = 32
+        btnCustomers.ImageAlign = ContentAlignment.MiddleLeft
+        btnCustomers.Location = New Point(0, 369)
+        btnCustomers.Margin = New Padding(4, 3, 4, 3)
+        btnCustomers.Name = "btnCustomers"
+        btnCustomers.Padding = New Padding(12, 0, 23, 0)
+        btnCustomers.Size = New Size(257, 69)
+        btnCustomers.TabIndex = 4
+        btnCustomers.Text = "Customers"
+        btnCustomers.TextAlign = ContentAlignment.MiddleLeft
+        btnCustomers.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnCustomers.UseVisualStyleBackColor = True
+        ' 
+        ' btnProducts
+        ' 
+        btnProducts.Dock = DockStyle.Top
+        btnProducts.FlatAppearance.BorderSize = 0
+        btnProducts.FlatStyle = FlatStyle.Flat
+        btnProducts.ForeColor = Color.Gainsboro
+        btnProducts.IconChar = FontAwesome.Sharp.IconChar.Tag
+        btnProducts.IconColor = Color.Gainsboro
+        btnProducts.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnProducts.IconSize = 32
+        btnProducts.ImageAlign = ContentAlignment.MiddleLeft
+        btnProducts.Location = New Point(0, 300)
+        btnProducts.Margin = New Padding(4, 3, 4, 3)
+        btnProducts.Name = "btnProducts"
+        btnProducts.Padding = New Padding(12, 0, 23, 0)
+        btnProducts.Size = New Size(257, 69)
+        btnProducts.TabIndex = 3
+        btnProducts.Text = "Products"
+        btnProducts.TextAlign = ContentAlignment.MiddleLeft
+        btnProducts.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnProducts.UseVisualStyleBackColor = True
+        ' 
+        ' btnOrders
+        ' 
+        btnOrders.Dock = DockStyle.Top
+        btnOrders.FlatAppearance.BorderSize = 0
+        btnOrders.FlatStyle = FlatStyle.Flat
+        btnOrders.ForeColor = Color.Gainsboro
+        btnOrders.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart
+        btnOrders.IconColor = Color.Gainsboro
+        btnOrders.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnOrders.IconSize = 32
+        btnOrders.ImageAlign = ContentAlignment.MiddleLeft
+        btnOrders.Location = New Point(0, 231)
+        btnOrders.Margin = New Padding(4, 3, 4, 3)
+        btnOrders.Name = "btnOrders"
+        btnOrders.Padding = New Padding(12, 0, 23, 0)
+        btnOrders.Size = New Size(257, 69)
+        btnOrders.TabIndex = 2
+        btnOrders.Text = "Orders"
+        btnOrders.TextAlign = ContentAlignment.MiddleLeft
+        btnOrders.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnOrders.UseVisualStyleBackColor = True
+        ' 
+        ' btnDashboard
+        ' 
+        btnDashboard.Dock = DockStyle.Top
+        btnDashboard.FlatAppearance.BorderSize = 0
+        btnDashboard.FlatStyle = FlatStyle.Flat
+        btnDashboard.ForeColor = Color.Gainsboro
+        btnDashboard.IconChar = FontAwesome.Sharp.IconChar.LineChart
+        btnDashboard.IconColor = Color.Gainsboro
+        btnDashboard.IconFont = FontAwesome.Sharp.IconFont.Auto
+        btnDashboard.IconSize = 32
+        btnDashboard.ImageAlign = ContentAlignment.MiddleLeft
+        btnDashboard.Location = New Point(0, 162)
+        btnDashboard.Margin = New Padding(4, 3, 4, 3)
+        btnDashboard.Name = "btnDashboard"
+        btnDashboard.Padding = New Padding(12, 0, 23, 0)
+        btnDashboard.Size = New Size(257, 69)
+        btnDashboard.TabIndex = 1
+        btnDashboard.Text = "Dashboard"
+        btnDashboard.TextAlign = ContentAlignment.MiddleLeft
+        btnDashboard.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnDashboard.UseVisualStyleBackColor = True
+        ' 
+        ' PanelLogo
+        ' 
+        PanelLogo.Controls.Add(ImgHome)
+        PanelLogo.Dock = DockStyle.Top
+        PanelLogo.Location = New Point(0, 0)
+        PanelLogo.Margin = New Padding(4, 3, 4, 3)
+        PanelLogo.Name = "PanelLogo"
+        PanelLogo.Size = New Size(257, 162)
+        PanelLogo.TabIndex = 0
+        ' 
+        ' ImgHome
+        ' 
+        ImgHome.Image = My.Resources.Resources.logo_Paltarumi
+        ImgHome.Location = New Point(26, 36)
+        ImgHome.Margin = New Padding(4, 3, 4, 3)
+        ImgHome.Name = "ImgHome"
+        ImgHome.Size = New Size(208, 92)
+        ImgHome.SizeMode = PictureBoxSizeMode.Zoom
+        ImgHome.TabIndex = 0
+        ImgHome.TabStop = False
+        ' 
+        ' PanelTitleBar
+        ' 
+        PanelTitleBar.BackColor = Color.FromArgb(CByte(26), CByte(25), CByte(62))
+        PanelTitleBar.Controls.Add(btnMinimize)
+        PanelTitleBar.Controls.Add(btnMaximize)
+        PanelTitleBar.Controls.Add(btnExit)
+        PanelTitleBar.Controls.Add(lblFormTitle)
+        PanelTitleBar.Controls.Add(IconCurrentForm)
+        PanelTitleBar.Dock = DockStyle.Top
+        PanelTitleBar.Location = New Point(257, 0)
+        PanelTitleBar.Margin = New Padding(4, 3, 4, 3)
+        PanelTitleBar.Name = "PanelTitleBar"
+        PanelTitleBar.Size = New Size(740, 87)
+        PanelTitleBar.TabIndex = 3
+        ' 
+        ' btnMinimize
+        ' 
+        btnMinimize.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnMinimize.FlatAppearance.BorderSize = 0
+        btnMinimize.FlatStyle = FlatStyle.Flat
+        btnMinimize.Image = My.Resources.Resources.minimize_icon_24
+        btnMinimize.Location = New Point(651, 0)
+        btnMinimize.Margin = New Padding(4, 3, 4, 3)
+        btnMinimize.Name = "btnMinimize"
+        btnMinimize.Size = New Size(30, 30)
+        btnMinimize.TabIndex = 4
+        btnMinimize.UseVisualStyleBackColor = True
+        ' 
+        ' btnMaximize
+        ' 
+        btnMaximize.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnMaximize.FlatAppearance.BorderSize = 0
+        btnMaximize.FlatStyle = FlatStyle.Flat
+        btnMaximize.Image = My.Resources.Resources.icon_maximize_24
+        btnMaximize.Location = New Point(680, 0)
+        btnMaximize.Margin = New Padding(4, 3, 4, 3)
+        btnMaximize.Name = "btnMaximize"
+        btnMaximize.Size = New Size(30, 30)
+        btnMaximize.TabIndex = 3
+        btnMaximize.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnExit.FlatAppearance.BorderSize = 0
+        btnExit.FlatStyle = FlatStyle.Flat
+        btnExit.Image = My.Resources.Resources.exit_icon_24
+        btnExit.Location = New Point(709, 0)
+        btnExit.Margin = New Padding(4, 3, 4, 3)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(30, 30)
+        btnExit.TabIndex = 2
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' lblFormTitle
+        ' 
+        lblFormTitle.AutoSize = True
+        lblFormTitle.ForeColor = Color.Gainsboro
+        lblFormTitle.Location = New Point(54, 36)
+        lblFormTitle.Margin = New Padding(4, 0, 4, 0)
+        lblFormTitle.Name = "lblFormTitle"
+        lblFormTitle.Size = New Size(40, 15)
+        lblFormTitle.TabIndex = 1
+        lblFormTitle.Text = "Home"
+        ' 
+        ' IconCurrentForm
+        ' 
+        IconCurrentForm.BackColor = Color.FromArgb(CByte(26), CByte(25), CByte(62))
+        IconCurrentForm.ForeColor = Color.MediumPurple
+        IconCurrentForm.IconChar = FontAwesome.Sharp.IconChar.HomeLg
+        IconCurrentForm.IconColor = Color.MediumPurple
+        IconCurrentForm.IconFont = FontAwesome.Sharp.IconFont.Auto
+        IconCurrentForm.IconSize = 37
+        IconCurrentForm.Location = New Point(10, 24)
+        IconCurrentForm.Margin = New Padding(4, 3, 4, 3)
+        IconCurrentForm.Name = "IconCurrentForm"
+        IconCurrentForm.Size = New Size(37, 37)
+        IconCurrentForm.TabIndex = 0
+        IconCurrentForm.TabStop = False
+        ' 
+        ' PanelDesktop
+        ' 
+        PanelDesktop.BackColor = Color.FromArgb(CByte(34), CByte(33), CByte(74))
+        PanelDesktop.Controls.Add(PictureBox1)
+        PanelDesktop.Dock = DockStyle.Fill
+        PanelDesktop.Location = New Point(257, 87)
+        PanelDesktop.Margin = New Padding(4, 3, 4, 3)
+        PanelDesktop.Name = "PanelDesktop"
+        PanelDesktop.Size = New Size(740, 514)
+        PanelDesktop.TabIndex = 4
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.None
+        PictureBox1.Image = My.Resources.Resources.logo_Paltarumi
+        PictureBox1.Location = New Point(140, 125)
+        PictureBox1.Margin = New Padding(4, 3, 4, 3)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(495, 284)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(997, 601)
+        Controls.Add(PanelDesktop)
+        Controls.Add(PanelTitleBar)
+        Controls.Add(PanelMenu)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "Form1"
+        PanelMenu.ResumeLayout(False)
+        PanelLogo.ResumeLayout(False)
+        CType(ImgHome, ComponentModel.ISupportInitialize).EndInit()
+        PanelTitleBar.ResumeLayout(False)
+        PanelTitleBar.PerformLayout()
+        CType(IconCurrentForm, ComponentModel.ISupportInitialize).EndInit()
+        PanelDesktop.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PanelMenu As Panel
+    Friend WithEvents PanelLogo As Panel
+    Friend WithEvents btnDashboard As FontAwesome.Sharp.IconButton
+    Friend WithEvents ImgHome As PictureBox
+    Friend WithEvents btnSetting As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnMarketing As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnCustomers As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnProducts As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnOrders As FontAwesome.Sharp.IconButton
+    Friend WithEvents PanelTitleBar As Panel
+    Friend WithEvents lblFormTitle As Label
+    Friend WithEvents IconCurrentForm As FontAwesome.Sharp.IconPictureBox
+    Friend WithEvents PanelDesktop As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnMinimize As Button
+    Friend WithEvents btnMaximize As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ WinFormsFuture/Form1.vb
@@ -0,0 +1,180 @@
+﻿Imports FontAwesome.Sharp
+Imports System.Runtime.InteropServices
+Imports System.Xml
+
+Public Class Form1
+    'Fields'
+    Private currentBtn As IconButton
+    Private leftBorderBtn As Panel
+    Private currentChildForm As Form
+    'Constructor'
+    Public Sub New()
+        ' This call is required by the designer.'
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.'
+        leftBorderBtn = New Panel()
+        leftBorderBtn.Size = New Size(7, 60)
+        PanelMenu.Controls.Add(leftBorderBtn)
+        'Form'
+        Me.Text = String.Empty
+        Me.ControlBox = False
+        Me.DoubleBuffered = True
+        'Me.MaximizedBounds = Screen.PrimaryScreen.WorkingArea
+    End Sub
+    'Methods'
+    Private Sub ActivateButton(senderBtn As Object, customColor As Color)
+        If senderBtn IsNot Nothing Then
+            DisableButton()
+            'Button'
+            currentBtn = CType(senderBtn, IconButton)
+            currentBtn.BackColor = Color.FromArgb(37, 36, 81)
+            currentBtn.ForeColor = customColor
+            currentBtn.IconColor = customColor
+            currentBtn.TextAlign = ContentAlignment.MiddleCenter
+            currentBtn.ImageAlign = ContentAlignment.MiddleRight
+            currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage
+            'Left Border'
+            leftBorderBtn.BackColor = customColor
+            leftBorderBtn.Location = New Point(0, currentBtn.Location.Y)
+            leftBorderBtn.Visible = True
+            leftBorderBtn.BringToFront()
+            'current Form icon'
+            IconCurrentForm.IconChar = currentBtn.IconChar
+            IconCurrentForm.IconColor = customColor
+        End If
+    End Sub
+    Private Sub DisableButton()
+        If currentBtn IsNot Nothing Then
+            currentBtn.BackColor = Color.FromArgb(31, 30, 68)
+            currentBtn.ForeColor = Color.Gainsboro
+            currentBtn.IconColor = Color.Gainsboro
+            currentBtn.TextAlign = ContentAlignment.MiddleLeft
+            currentBtn.ImageAlign = ContentAlignment.MiddleLeft
+            currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText
+        End If
+    End Sub
+
+    Private Sub OpenChildForm(childForm As Form)
+        'Open only form'
+        If currentChildForm IsNot Nothing Then
+            currentChildForm.Close()
+        End If
+        currentChildForm = childForm
+        'end'
+        childForm.TopLevel = False
+        childForm.FormBorderStyle = FormBorderStyle.None
+        childForm.Dock = DockStyle.Fill
+        PanelDesktop.Controls.Add(childForm)
+        PanelDesktop.Tag = childForm
+        childForm.BringToFront()
+        childForm.Show()
+        lblFormTitle.Text = childForm.Text
+    End Sub
+
+    Private Sub Reset()
+        DisableButton()
+        leftBorderBtn.Visible = False
+        IconCurrentForm.IconChar = IconChar.Home
+        IconCurrentForm.IconColor = Color.MediumPurple
+        lblFormTitle.Text = "Home"
+    End Sub
+
+    'Events'
+    'Reset'
+    Private Sub imgHome_Click(sender As Object, e As EventArgs) Handles ImgHome.Click
+        If currentChildForm IsNot Nothing Then
+            currentChildForm.Close()
+        End If
+        Reset()
+    End Sub
+
+    'Menu buttons Cliks'
+    Private Sub btnDashboard_Click(sender As Object, e As EventArgs) Handles btnDashboard.Click
+        ActivateButton(sender, RGBColors.color1)
+        lblFormTitle.Text = btnDashboard.Text
+        'OpenChildForm(New FormDashboard)
+    End Sub
+
+    Private Sub btnOrders_Click(sender As Object, e As EventArgs) Handles btnOrders.Click
+        ActivateButton(sender, RGBColors.color2)
+        lblFormTitle.Text = btnOrders.Text
+        'OpenChildForm(New FormOrders)
+    End Sub
+
+    Private Sub btnProducts_Click(sender As Object, e As EventArgs) Handles btnProducts.Click
+        ActivateButton(sender, RGBColors.color3)
+        lblFormTitle.Text = btnProducts.Text
+        'OpenChildForm(New FormProducts)
+    End Sub
+
+    Private Sub btnCustomers_Click(sender As Object, e As EventArgs) Handles btnCustomers.Click
+        ActivateButton(sender, RGBColors.color4)
+        lblFormTitle.Text = btnCustomers.Text
+        'OpenChildForm(New FormCustomers)
+    End Sub
+
+    Private Sub btnMarketing_Click(sender As Object, e As EventArgs) Handles btnMarketing.Click
+        ActivateButton(sender, RGBColors.color5)
+        lblFormTitle.Text = btnMarketing.Text
+        'OpenChildForm(New FormMarketing)
+    End Sub
+
+    Private Sub btnSetting_Click(sender As Object, e As EventArgs) Handles btnSetting.Click
+        ActivateButton(sender, RGBColors.color6)
+        lblFormTitle.Text = btnSetting.Text
+        'OpenChildForm(New FormSetting)
+    End Sub
+
+    'Drag Form'
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+
+    Private Sub PanelTitleBar_MouseDown(sender As Object, e As MouseEventArgs) Handles PanelTitleBar.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+    'Close-Maximize-Minimize'
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnMaximize_Click(sender As Object, e As EventArgs) Handles btnMaximize.Click
+        If WindowState = FormWindowState.Normal Then
+            WindowState = FormWindowState.Maximized
+        Else
+            WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub btnMinimize_Click(sender As Object, e As EventArgs) Handles btnMinimize.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    'Remove transparent border in maximized state'
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If WindowState = FormWindowState.Maximized Then
+            FormBorderStyle = FormBorderStyle.None
+        Else
+            FormBorderStyle = FormBorderStyle.Sizable
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'FormBorderStyle = FormBorderStyle.None
+    End Sub
+End Class
+
+Public Structure RGBColors
+    Public Shared color1 As Color = Color.FromArgb(172, 126, 241)
+    Public Shared color2 As Color = Color.FromArgb(249, 118, 176)
+    Public Shared color3 As Color = Color.FromArgb(253, 138, 114)
+    Public Shared color4 As Color = Color.FromArgb(95, 77, 221)
+    Public Shared color5 As Color = Color.FromArgb(249, 88, 155)
+    Public Shared color6 As Color = Color.FromArgb(24, 161, 251)
+End Structure
